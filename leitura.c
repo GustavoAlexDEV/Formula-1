0feae1fd74fdfefed67c4a8315f706bb515fbabe
GustavoAlexDEV <146300786+GustavoAlexDEV@users.noreply.github.com>
2025-01-20 09:29:09 -0300

    Update leitura.c

--- leitura.c
+++ leitura.c
@@ -6,10 +6,13 @@
 void le_estrutura(FILE *arq,*Pilotos,*Equipes,int n){
     int aux=0,i,j,c=0;
     char Saux1[32];
+    char Saux2[32]
     for(i=0;i<n;i++){
-        fscanf(ENTRADA,"%s %s",Pilotos[i],Saux1);
+        fscanf(ENTRADA,"%s %s",Saux1,Saux2);
+        strcpy(Saux1,Pilotos[i].nome);
+        strcpy(Saux2,Pilotos[i].equipe);
         for(j=0;j<c;j++){
-            if(strcmp(Saux1,Equipes[j])==0){
+            if(strcmp(Saux2,Equipes[j].nome)==0){
                 Endereco[i]=j;
                 aux=1;
                 break;
