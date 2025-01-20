4f62d8fe7b573da729df4d6e13dc5755f2fe1910
GustavoAlexDEV <146300786+GustavoAlexDEV@users.noreply.github.com>
2025-01-20 10:57:46 -0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "leitura.h"
+#include "escrita.h"
+#include "registros.h"
+int main(int argc,char *argv[]){
+  int n,m;
+  char arqv[]="ENTRADA";
+  FILE* arq=fopen(arqv);
+  return 0;
+}
