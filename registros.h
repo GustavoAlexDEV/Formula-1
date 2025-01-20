4e64c138c729644e7f22947cf6ae03df8368589b
GustavoAlexDEV <146300786+GustavoAlexDEV@users.noreply.github.com>
2025-01-20 09:43:28 -0300

    Add files via upload

---
+++ registros.h
@@ -0,0 +1,20 @@
+
+#ifndef REGISTROS_H
+#define REGISTROS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+        char nome[32];
+        char equipe[32];
+        int colocacao[10];
+        int pontos;
+    }Estrutura_Pilotos;
+    typedef struct{
+        char *nome;
+        int *colocacao;
+        int pontos;
+    }Estrutura_Equipes;
+
+#endif
