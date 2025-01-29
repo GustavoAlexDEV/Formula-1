e71f1a477844da230fa91a55403b2577708316a2
GustavoAlexDEV <146300786+GustavoAlexDEV@users.noreply.github.com>
2025-01-29 16:49:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,18 +10,18 @@ int main(int argc,char *argv[]){
   int n,m;
   int tabela[10]={25,18,15,12,10,8,6,4,2,1};
   char aux[32];
+  FILE* arq1=fopen(argv[1],"r");
+  FILE* arq2=fopen(argv[2],"w");
+  //escaneia o numero de pilotos e de partidas
   fscanf(arq1,"%d %d",&n,&m);
   Estrutura_Equipes* Equipes = malloc(sizeof(Estrutura_Equipes)*n);
   Estrutura_Pilotos* Pilotos = malloc(sizeof(Estrutura_Pilotos)*n);
-  FILE* arq1=fopen(argv[1],"r");
-  FILE* arq2=fopen(argv[2],"w");
-  //escaneia o numero de jogadores e de partidas
   le_estrutura(arq1,Pilotos,Equipes,n);
   //le ---
   fscanf(arq1,"%s",aux);
-  //le n jogadores m vezes
+  //le n pilotos m vezes
   le_partidas(arq1,Pilotos,Equipes,tabela,n,m);
-  //ordena os n jogadores
+  //ordena os n pilotos
   calcula_resultado(Pilotos,Equipes,n);
   //salva o resultado no arquivo endereçado em arq2
   escrever_resultados(arq2,Pilotos,Equipes,n);
