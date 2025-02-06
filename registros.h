c1ed492730771cb5b791df83b1ddb062e70bbfce
PumppiePullman <167942983+PumppiePullman@users.noreply.github.com>
2025-02-06 12:55:41 -0300

    Update registros.h

--- registros.h
+++ registros.h
@@ -11,7 +11,7 @@ typedef struct{
         int colocacao[40];
         int pontos;
     }Estrutura_Pilotos;
-    typedef struct{
+typedef struct{
         char nome[32];
         int colocacao[40];
         int pontos;
